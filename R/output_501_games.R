ad52388128354355e8405267fc6066ee0b706633
Alun Hewinson <alunhewinson@gmail.com>
2023-03-21 22:22:41 +0000

    remove some unneeded data and update readme

--- R/output_501_games.R
+++ R/output_501_games.R
@@ -1,7 +1,6 @@
-library(ggplot2)
 library(dbplyr)
 library(RSQLite)
-library(tidyverse)
+library(readr)
 
 mdt_file <- "C:/Documents and Settings/alunh/Documents/repos/binary_data/darts/db_latest.mdt"
 
@@ -11,65 +10,16 @@ con <- dbConnect(drv=RSQLite::SQLite(), dbname=mdt_file)
 ## list all tables
 tables <- dbListTables(con)
 
-nl <- sapply(tables, function(q) {
-  qq <- dbReadTable(con, q)
-  if (nrow(qq) > 0) {
-    cat(q)
-    cat(" = ")
-    cat(nrow(qq))
-    cat(" rows\n")
-  }
-})
+game_501 <- dbReadTable(con, "aufnahme")
 
-# Finish = 10 rows
-# HalveIt = 2 rows
-# HalveItRound = 19 rows
-# OneOrTen = 1 rows
-# Profile = 1 rows
-# android_metadata = 1 rows
-# aufnahme = 1893 rows
-# dartTarget = 1520 rows
-# playTime = 185 rows
-# rtw = 73 rows
-# xGame = 111 rows
+unfinished_or_lost <- game_501 %>% 
+  group_by(entityId) %>% 
+  filter(round == max(round)) %>% 
+  filter(beginnScore != score) %>% 
+  select(entityId) %>% ungroup()
 
-aufnahme <- dbReadTable(con, "aufnahme")
-# aufnahme %>% 
-#   as_tibble() %>% 
-#   dplyr::select(entityName) %>% 
-#   unique
+completed_games <- game_501 %>% 
+  anti_join(unfinished_or_lost) %>% 
+  select(-entityName, -profileId)
 
-dartTarget <- dbReadTable(con, "dartTarget")
-# dartTarget %>% 
-#   dplyr::select(entityName) %>% 
-#   unique
-
-playTime <- dbReadTable(con, "playTime")
-playTime
-
-xGame <- dbReadTable(con, "xGame")
-xGame %>% 
-  arrange(gesamtDarts) %>% 
-  dplyr::select(gesamtDarts) %>%
-  unlist() %>% hist()
-#plot(type="l")
-
-xGame %>% 
-  ggplot(aes(id, gesamtDarts)) +
-  geom_line() + 
-  geom_smooth()
-
-##### demonstration of selection bias
-## media
-
-gameScores <- xGame %>% sample_frac(1) %>% select(gesamtDarts) %>% unlist()
-
-allMedian <- gameScores %>% median()
-randomGames <- gameScores %>% matrix(2) %>% t() %>% as.data.frame() 
-randomGames %>% apply(1, min) %>% median()
-
-library(fitdistrplus)
-descdist(gameScores, discrete=FALSE, boot=500)
-fitdist(gameScores, "gamma")
-#fitdist(gameScores, "beta")
-
+write_csv(completed_games, file = "data/completed_501_games.csv")
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # darts_practice
 A log of all my darts practice games
+
+## data included
+
+### data/completed_501_games.csv
+id = a unique row identifier
+round = an id for each round of (up to) three darts unique to each game
+beginnScore = the start score before this round
+score = the score achieved this round
+darts = the number of darts used this round
+dartsOnDouble = how many attempts were made on a checkout double this round
+checkout = whether checkout was achieved in this round
+entityId = a unique game id
+created_at = a datetime for the end of the game
+
